36bd313a4394da92c70e6439bba7a75925f12b8a
Scott Motte <mot@mot.la>
2023-11-24 15:40:39 -0700

    /VERSION

--- index.js
+++ index.js
@@ -41,6 +41,27 @@ app.get('/:os/:arch', async (req, res) => {
   }
 })
 
+app.get('/VERSION', async (req, res) => {
+  // Construct the URL to proxy the request to /releases/VERSION
+  const proxyUrl = 'https://dotenvx.com/releases/VERSION'
+
+  try {
+    // Using axios to get a response stream
+    const response = await axios.get(proxyUrl, {
+      responseType: 'stream'
+    })
+
+    // Setting headers for the response
+    res.setHeader('Content-Type', response.headers['content-type'])
+    res.setHeader('Content-Length', response.headers['content-length'])
+
+    // Piping the response stream to the client
+    response.data.pipe(res)
+  } catch (error) {
+    res.status(500).send('Error occurred while fetching the file: ' + error.message)
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server is running on http://:${port}`)
 })
