d4d15dfa3f790967771402d8604d8034b8825f33
Scott Motte <mot@mot.la>
2023-11-24 15:30:48 -0700

    🏡

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const axios = require('axios')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+  res.send('dotenvx.sh')
+})
+
 app.get('/:os/:arch', async (req, res) => {
   const os = req.params.os.toLowerCase()
   const arch = req.params.arch.toLowerCase()
