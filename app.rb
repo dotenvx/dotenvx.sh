1f2269e35b88a544897b754239c4d15b7c7e30e2
Scott Motte <mot@mot.la>
2026-05-14 13:00:58 -0700

    remove enterprise

--- app.rb
+++ app.rb
@@ -31,16 +31,6 @@ RADAR_VERSION = begin
 rescue
   "0.1.0"
 end
-DOTENV_ENTERPRISE_INSTALL_SCRIPT = begin
-  File.read(File.join(__dir__, "dotenv-enterprise/install.sh"))
-rescue
-  ""
-end
-DOTENV_ENTERPRISE_VERSION = begin
-  File.read(File.join(__dir__, "dotenv-enterprise/VERSION")).strip
-rescue
-  "0.1.0"
-end
 EXT_HUB_INSTALL_SCRIPT = begin
   File.read(File.join(__dir__, "ext/hub/install.sh"))
 rescue
@@ -189,22 +179,6 @@ end
 get "/radar/VERSION" do
   content_type "text/plain"
   RADAR_VERSION
-end
-
-# for dotenv-enterprise
-get "/dotenv-enterprise" do
-  content_type "text/plain"
-  handle_install(DOTENV_ENTERPRISE_INSTALL_SCRIPT, params["version"], params["directory"], params["os"], params["arch"], params["force"])
-end
-
-get "/dotenv-enterprise/install.sh" do
-  content_type "text/plain"
-  handle_install(DOTENV_ENTERPRISE_INSTALL_SCRIPT, params["version"], params["directory"], params["os"], params["arch"], params["force"])
-end
-
-get "/dotenv-enterprise/VERSION" do
-  content_type "text/plain"
-  DOTENV_ENTERPRISE_VERSION
 end
 
 # for ext/hub
